705071459ce9f127e01adb97ccf79a801f905fd2
sepid <sepideh7sadati@gmail.com>
2022-07-10 23:16:04 +0430

    change-comment

--- src/main/java/com/tecnotree/TecnoTreeApplication.java
+++ src/main/java/com/tecnotree/TecnoTreeApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * The class TecnoTreeApplication run project.
+ */
 @SpringBootApplication
 @EnableFeignClients
 public class TecnoTreeApplication {
